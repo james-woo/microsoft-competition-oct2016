27fd3093a53365e835b66988e59ffb620751e344
Dana P <Dana P>
2016-10-11 21:50:45 -0700

    Added problem descriptions and an output

--- problemThree.py
+++ problemThree.py
@@ -1,5 +1,17 @@
 import os
 
+"""
+In ths question, we were given a list of colours. This colours represented the second row in a painting. The goal of this program is to find the first row of that painting. We know that the second row is composed of combinations of the above 2 colours from the first row. For example: 
+Row 1: Yellow Blank Red Blue Yellow Blank Blue
+would result in a Row 2: 
+Yellow(Y) Yellow(Y+B) Red(B+R) Purple(R+B) Green(B+Y) Yellow(Y+B) Blue(B+B) Blue(B)
+
+The second row has one more colour than the first row. The first and last colours in the second row are just the first and last colours in the first row. 
+We are also told that the first row can only be composed of 3 colours (red, yellow, blue) and blank. 
+
+The input lines below are the second row of the painting.
+"""
+
 #input = "red purple green orange purple purple orange yellow yellow yellow" 
 input = "blue blue yellow orange orange green green green blue blue purple red red orange orange purple green yellow red red yellow orange orange green purple purple purple purple green orange red yellow yellow blue blue yellow yellow yellow orange purple green yellow blue purple purple blue red purple green orange red yellow yellow blue purple purple blue yellow green green yellow red purple green green blue red orange yellow blue blue red purple blue yellow yellow red red yellow green purple purple green orange orange orange orange yellow yellow green green yellow red purple blue red red red red blank"
 
@@ -29,7 +41,7 @@ for i in range(1, len(inputArr)-1):
 print inputArr[-1];
 
 print '-------------------------'
-with open("output2.txt", "a") as myfile:
+with open("output3.txt", "a") as myfile:
     for color in outputArr:
         print color + " ",
         myfile.write(color + " ")
--- problemTwo.py
+++ problemTwo.py
@@ -1,11 +1,18 @@
 import os
 
+"""
+In this problem, we were given a file with numbers. The first number in the file represents the number of lines that follow (e.g. could be used to instantiate array size). The following rows are a bunch of numbers. In each row there's a bunch of numbers. First find the largest and the smallest numbers from these rows. The goal of this program is to find the missing numbers between that largest and smallest. For example, if a row contains: 
+6 5 2 1
+the output should have 3 4 (the missing numbers between 1 and 6)
+If a row contains
+4 2 1 3
+the output should be a blank line as there are no missing numbers between 1 and 4. 
+"""
+
 arr = open("PracticeInput2a.txt").readlines()
 #arr = open("input2.txt").readlines()
 
 with open("output2.txt", "a") as myfile:
-    
-
     for x in range(1, len(arr)):
         line = arr[x];
         numbers = line.replace('\n','').split(" ");
